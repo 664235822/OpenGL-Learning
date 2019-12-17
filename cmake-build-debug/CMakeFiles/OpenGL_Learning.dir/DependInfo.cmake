2d7c412122a7401e54df1bee9d4e9fafd0ca00b2
SmallPig1997 <664235822@qq.com>
2019-12-18 07:51:04 +0800

    搭建环境

---
+++ cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Volumes/Projects/OpenGL-Learning/src/glad.c" "/Volumes/Projects/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/glew/2.1.0_1/include/GL"
+  "/usr/local/Cellar/glfw/3.3/include/GLFW"
+  "/usr/local/include/glad"
+  "/usr/local/include/KHR"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Volumes/Projects/OpenGL-Learning/main.cpp" "/Volumes/Projects/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/glew/2.1.0_1/include/GL"
+  "/usr/local/Cellar/glfw/3.3/include/GLFW"
+  "/usr/local/include/glad"
+  "/usr/local/include/KHR"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_Learning.dir/main.cpp.o"
+  "CMakeFiles/OpenGL_Learning.dir/src/glad.c.o"
+  "OpenGL_Learning"
+  "OpenGL_Learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL_Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
