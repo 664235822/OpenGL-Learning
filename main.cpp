920d84e4be4b06e3363811729252ed1644a0d820
SmallPig1997 <664235822@qq.com>
2019-12-30 11:25:17 +0800

    创建窗口2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <glfw3.h>
 #include <iostream>
 
+void processInput(GLFWwindow *window);
+
 int main(void) {
     //初始化GLFW
     if (!glfwInit()) {
@@ -35,6 +37,11 @@ int main(void) {
 
     //渲染循环
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -43,4 +50,11 @@ int main(void) {
     printf("Exit Application");
     glfwTerminate();
     return 0;
+}
+
+//进程输入
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
