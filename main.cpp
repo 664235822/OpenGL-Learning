4cb1e2a143a55ce96c01b587e9bafe7df0318bee
SmallPig1997 <664235822@qq.com>
2020-01-10 13:48:55 +0800

    创建三角形、矩形

--- main.cpp
+++ main.cpp
@@ -2,6 +2,29 @@
 #include <glfw3.h>
 #include <iostream>
 
+float vertices[] = {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f
+};
+
+unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+};
+
+
+const char *vertexShaderSource = "#version 330 core \n"
+                                 "layout (location = 0) in vec3 aPos; \n"
+                                 "void main(){\n"
+                                 "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main(){\n"
+                                   "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}";
+
 void processInput(GLFWwindow *window);
 
 int main(void) {
@@ -35,12 +58,62 @@ int main(void) {
     //视口
     glViewport(0, 0, 800, 600);
 
+    //背面剔除
+    //glEnable(GL_CULL_FACE);
+    //glCullFace(GL_BACK);
+
+    //创建VAO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    //创建VBO
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //创建EBO
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //编译vertexShader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    //编译fragmentShader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    //创建shader程序
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
     //渲染循环
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //画三角形、矩形
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glUseProgram(shaderProgram);
+        //glDrawArrays(GL_TRIANGLES,0,3);//三角形
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);//矩形
 
         glfwSwapBuffers(window);
         glfwPollEvents();
