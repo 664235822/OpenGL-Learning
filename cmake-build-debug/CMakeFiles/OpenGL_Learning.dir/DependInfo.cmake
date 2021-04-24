770a4a4f2c7193b37a81c8b7a7b06585a142b737
SmallPig1997 <664235822@qq.com>
2021-04-24 22:51:22 +0800

    更新GLFW版本

--- cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/DependInfo.cmake
@@ -5,36 +5,36 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Volumes/项目/OpenGL-Learning/src/glad.c" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/glad.c.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/glad.c" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/glad.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
   "/usr/local/Cellar/glew/2.2.0_1/include/GL"
-  "/usr/local/Cellar/glfw/3.3.2/include/GLFW"
+  "/usr/local/Cellar/glfw/3.3.4/include/GLFW"
   "/usr/local/Cellar/assimp/5.0.1/include"
   "/usr/local/include/glad"
   "/usr/local/include/KHR"
   "/usr/local/Cellar/glm/0.9.9.8/include/glm"
   )
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Volumes/项目/OpenGL-Learning/main.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/main.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/Camera.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Camera.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/DirectionLight.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/DirectionLight.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/Material.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Material.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/Mesh.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Mesh.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/Model.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Model.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/PointLight.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/PointLight.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/Shader.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Shader.cpp.o"
-  "/Volumes/项目/OpenGL-Learning/src/SpotLight.cpp" "/Volumes/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/SpotLight.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/main.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/main.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/Camera.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Camera.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/DirectionLight.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/DirectionLight.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/Material.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Material.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/Mesh.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Mesh.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/Model.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Model.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/PointLight.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/PointLight.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/Shader.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/Shader.cpp.o"
+  "/Volumes/资料/项目/OpenGL-Learning/src/SpotLight.cpp" "/Volumes/资料/项目/OpenGL-Learning/cmake-build-debug/CMakeFiles/OpenGL_Learning.dir/src/SpotLight.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "/usr/local/Cellar/glew/2.2.0_1/include/GL"
-  "/usr/local/Cellar/glfw/3.3.2/include/GLFW"
+  "/usr/local/Cellar/glfw/3.3.4/include/GLFW"
   "/usr/local/Cellar/assimp/5.0.1/include"
   "/usr/local/include/glad"
   "/usr/local/include/KHR"
